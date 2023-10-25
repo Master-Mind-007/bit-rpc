3b64f829a52583a82a23869a14632f16d0e2f5c4
MasterMind <87894877+Master-Mind-007@users.noreply.github.com>
2023-10-26 01:01:41 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -24,15 +24,47 @@ sleep 10
 mv bitcoin.conf ~/.bitcoin/bitcoin.conf
 echo "Transfered Conf File!"
 
+# Function to check if bitcoind is running
+check_bitcoind_running() {
+  if pgrep -x "bitcoind" > /dev/null
+  then
+    return 0  # Bitcoind is running
+  else
+    return 1  # Bitcoind is not running
+  fi
+}
+
 # Start bitcoind in daemon mode
 bitcoind -daemon
 
 # Sleep for 20 seconds
-echo "Started Daemon | Taking 20s nap!"
-sleep 20
+echo "Started Daemon | Taking 10s nap!"
+sleep 10
+
+# Check if bitcoind is running
+if check_bitcoind_running
+then
+  echo "Bitcoind is running."
+else
+  echo "Bitcoind is not running. Trying to start it..."
+
+  # Use a while loop to keep trying to start bitcoind
+  while true
+  do
+    bitcoind -daemon
+    sleep 5
+
+    if check_bitcoind_running
+    then
+      echo "Bitcoind has been successfully started."
+      break  # Exit the loop once it's running
+    fi
+  done
+fi
 
 echo "Verifing Block Count:"
 bitcoin-cli getblockcount
+echo "BitCore Logs"
 tail ~/.bitcoin/debug.log
 
 
