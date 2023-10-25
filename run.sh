6f2d24dbbe9eb385152cf6387f5ba263fa19aced
MasterMind <87894877+Master-Mind-007@users.noreply.github.com>
2023-10-25 21:06:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,24 @@
+#!/bin/bash
+
+# Start bitcoind in daemon mode
 bitcoind -daemon
+
+# Sleep for 10 seconds
+sleep 10
+echo "Taking 10s nap!"
+
+# Stop the bitcoind server
+bitcoin-cli stop
+
+# Sleep for another 10 seconds
+sleep 10
+echo "Taking 10s nap!"
+
+# Start bitcoind again
+bitcoind -daemon
+
+# Sleep for another 10 seconds
+sleep 10
+echo "Taking 10s nap!"
+
 npm start
